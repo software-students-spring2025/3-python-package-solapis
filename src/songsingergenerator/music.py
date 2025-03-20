6e0421a90142db261b71dc7570720b2f02967ebe
ZiruiHan <zh2267@nyu.edu>
2025-03-20 14:02:11 -0400

    Updated music.py with more collection

--- src/songsingergenerator/music.py
+++ src/songsingergenerator/music.py
@@ -2,12 +2,52 @@ import random
 
 collection = {
     "rnb": {
-        "SZA": ["luther", "30 For 30", "Snooze", "Kill Bill"],
-        "Babyface": ["Every Time I Close My Eyes", "Lady, Lady", "Illusions"]
+        "SZA": ["Luther", "30 For 30", "Snooze", "Kill Bill", "Good Days"],
+        "Babyface": ["Every Time I Close My Eyes", "Lady, Lady", "Illusions", "When Can I See You Again"],
+        "Alicia Keys": ["No One", "If I Ain't Got You", "Fallin'"],
+        "Beyoncé": ["Irreplaceable", "Halo", "Drunk in Love"]
     },
     "rock": {
-        "Pink Floyd": ["Wish You Were Here", "Comfortably Numb", "Time"],
-        "The Beatles": ["Come Together", "Here Comes The Sun", "Here, There And Everywhere"]
+        "Pink Floyd": ["Wish You Were Here", "Comfortably Numb", "Time", "Shine On You Crazy Diamond"],
+        "The Beatles": ["Come Together", "Here Comes The Sun", "Hey Jude", "Let It Be"],
+        "The Rolling Stones": ["Paint It Black", "Gimme Shelter", "Sympathy for the Devil"],
+        "Led Zeppelin": ["Stairway to Heaven", "Whole Lotta Love", "Kashmir"]
+    },
+    "hip-hop": {
+        "Kendrick Lamar": ["HUMBLE.", "Alright", "Money Trees", "DNA."],
+        "Drake": ["God's Plan", "In My Feelings", "One Dance", "Hotline Bling"],
+        "J. Cole": ["No Role Modelz", "Love Yourz", "Wet Dreamz"],
+        "Kanye West": ["Stronger", "Gold Digger", "Flashing Lights"]
+    },
+    "pop": {
+        "Taylor Swift": ["Blank Space", "Love Story", "You Belong With Me", "Shake It Off"],
+        "Adele": ["Hello", "Someone Like You", "Rolling in the Deep", "Set Fire to the Rain"],
+        "Ed Sheeran": ["Shape of You", "Thinking Out Loud", "Perfect"],
+        "Dua Lipa": ["Don't Start Now", "Levitating", "New Rules"]
+    },
+    "jazz": {
+        "Miles Davis": ["So What", "Freddie Freeloader", "Blue in Green"],
+        "John Coltrane": ["Giant Steps", "My Favorite Things", "Blue Train"],
+        "Louis Armstrong": ["What a Wonderful World", "La Vie En Rose", "Stardust"],
+        "Ella Fitzgerald": ["Summertime", "Cheek to Cheek", "Dream a Little Dream of Me"]
+    },
+    "country": {
+        "Johnny Cash": ["Ring of Fire", "Hurt", "Folsom Prison Blues"],
+        "Dolly Parton": ["Jolene", "9 to 5", "Coat of Many Colors"],
+        "Willie Nelson": ["On the Road Again", "Always on My Mind", "Blue Eyes Crying in the Rain"],
+        "Luke Combs": ["Beautiful Crazy", "Hurricane", "Beer Never Broke My Heart"]
+    },
+    "electronic": {
+        "Daft Punk": ["One More Time", "Get Lucky", "Harder, Better, Faster, Stronger"],
+        "Calvin Harris": ["Summer", "Feel So Close", "This Is What You Came For"],
+        "Deadmau5": ["Strobe", "Ghosts 'n' Stuff", "Some Chords"],
+        "Zedd": ["Clarity", "Stay", "The Middle"]
+    },
+    "classical": {
+        "Ludwig van Beethoven": ["Symphony No. 9", "Für Elise", "Moonlight Sonata"],
+        "Wolfgang Amadeus Mozart": ["Eine kleine Nachtmusik", "Requiem", "The Magic Flute"],
+        "Johann Sebastian Bach": ["Toccata and Fugue in D Minor", "Brandenburg Concerto", "Air on the G String"],
+        "Pyotr Ilyich Tchaikovsky": ["Swan Lake", "The Nutcracker", "1812 Overture"]
     }
 }
 
